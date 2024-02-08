342d9f775ec36e1c458dcbc02be4527875d318fa
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2024-02-08 13:22:50 +0100

    added models

---
+++ Infrastructure/Entities/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string SubCategory { get; set; } = null!;
+    }
+}
---
+++ Infrastructure/Models/ContactCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class ContactCommand
+    {
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public string SubCategory { get; set; } = null!;
+        public Contact ParseToEntityModel()
+        {
+            return new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = Name,
+                Surname = Surname,
+                Password = Password,
+                Email = Email,
+                PhoneNumber = PhoneNumber,
+                Category = Category,
+                SubCategory = SubCategory
+            };
+        }
+    }
